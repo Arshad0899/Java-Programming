c4b1763a5fe4d5b85192aab0f1a6daf36ab48131
Arshad <arshadbasha1999@gmail.com>
2022-12-16 14:11:22 +0530

    Removed comments

--- day_4/Pattern_10.java
+++ day_4/Pattern_10.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Pattern_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the number: ");
