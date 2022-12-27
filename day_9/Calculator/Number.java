7003dd33f1c2d68c280b69e0d63edaaf9bf9093d
Arshad <arshadbasha1999@gmail.com>
2022-12-27 11:17:39 +0530

    changes in Number class

--- day_9/Calculator/Number.java
+++ day_9/Calculator/Number.java
@@ -6,11 +6,13 @@ public class Number {
 	private int num2;
 	private int result;
 	
+	// Constructor -->
 	public Number(int num1, int num2) {
 		this.num1 = num1;
 		this.num2 = num2;
 	}
 	
+	// Getters and Setter for num1 and num2 --> 
 	public void setNum1(int num1) {
 		this.num1 = num1;
 	}
@@ -27,6 +29,8 @@ public class Number {
 		return num2;
 	}
 
+
+	// methods -->
 	public void add() {
 		result = num1 + num2;
 	}
@@ -59,7 +63,7 @@ public class Number {
 		}
 	}
 
-
+	// Getters for result -->
 	public int getResult() {
 		return result;
 	}
